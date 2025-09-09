4a5cc5aa9de469f98d4533d6975795224617edc1
RandomGuy1661 <ericlap330@gmail.com>
2025-09-09 14:06:04 -0600

    finished project i think

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function Student(){
+    <div>
+        <p>Eric LaPierre</p>
+        <Link href="https://github.com/RandomGuy1661/cprg306-assignments">GitHub Repo</Link>
+    </div>
+} 
