fd0e5cae7975b9d5db195b69909593b6957a1941
Eric LaP <ericlap330@gmail.com>
2025-09-25 14:43:30 -0600

    touched up the main page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import Link from "next/link";
 
 export default function Home(){
+  let linkStyles = "text-blue-600 underline hover:text-cyan-400";
   return(
+
     <main>
       <h1>CPRG306 Assignments</h1>
       <ul>
-        <li><Link href="./week-2">Week 2 Assignment</Link></li>
-        <li><Link href="./week-3">Week 3 Assignment</Link></li>
-        <li><Link href="./week-4">Week 4 Assignment</Link></li>
+        <li><Link className={linkStyles} href="./week-2">Week 2 Assignment</Link></li>
+        <li><Link className={linkStyles} href="./week-3">Week 3 Assignment</Link></li>
+        <li><Link className={linkStyles} href="./week-4">Week 4 Assignment</Link></li>
       </ul>
     </main>
   ) 
